1c6e0b088438fba505672d0ed04270b090829e6a
raju-kr50 <raju_kr50@yahoo.com>
2019-07-12 14:38:48 +0530

    Added the test cases in PalindromeTest.java

--- src/main/java/com/stackroute/pe1/Palindrome.java
+++ src/main/java/com/stackroute/pe1/Palindrome.java
@@ -1,35 +1,43 @@
-//package com.stackroute.pe1;
-//
-//import java.util.Scanner;
-//
-//public class Palindrome {
-////    public static void main(String[] args) {
-////        int number, reversedNumber = 0, sum = 0, temp, remainder;
-////        Scanner sc = new Scanner(System.in);
-////        System.out.println("Enter a number: ");
-////        number = sc.nextInt();
-//
-//    public String checkPalindrome(int number) {
-//        if(number <= 0) {
-//            return "Enter number greater than 0";
-//        }
-//        temp = number;
-//        while (temp != 0) {
-//            remainder = temp % 10;
-//            if (remainder % 2 == 0)
-//                sum = sum + remainder;
-//            reversedNumber = reversedNumber * 10 + remainder;
-//            temp = temp / 10;
-//        }
-//
-//        if (reversedNumber == number) {
-//            System.out.println(number + " is a palindrome");
-//        } else {
-//            if (sum > 25) {
-//                System.out.println(number + " is not palindrome and the sum of even numbers is greater than 25");
-//            } else
-//                System.out.println(number + " is not palindrome and the sum of even numbers is not greater than 25");
-//        }
-//    }
-//}
-//
+package com.stackroute.pe1;
+
+import java.util.Scanner;
+
+public class Palindrome {
+//    public static void main(String[] args) {
+//        int number, reversedNumber = 0, sum = 0, temp, remainder;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number: ");
+//        number = sc.nextInt();
+
+    public String checkPalindrome(int number) {
+        if (number <= 0) {
+            return "Enter number greater than 0";
+        }
+
+        int temp = number;
+        int remainder;
+        int sum = 0;
+        int reversedNumber = 0;
+        String  result;
+        while (temp != 0) {
+            remainder = temp % 10;
+            if (remainder % 2 == 0)
+                sum = sum + remainder;
+            reversedNumber = reversedNumber * 10 + remainder;
+            temp = temp / 10;
+        }
+
+        if (reversedNumber == number) {
+        //    result = "Palindrome";
+             if (sum > 25) {
+                result = "The number is palindrome and the sum of even numbers is greater than 25";
+             } else {
+                result = "The number is palindrome and the sum of even numbers is not greater than 25";
+             }
+        } else {
+            result = "Not Palindrome";
+        }
+        return result;
+    }
+}
+
--- src/main/test/java/com/stackroute/pe1/PalindromeTest.java
+++ src/main/test/java/com/stackroute/pe1/PalindromeTest.java
@@ -1,37 +1,70 @@
-//package com.stackroute.pe1;
-//
-//import org.junit.*;
-//
-//import static org.junit.Assert.*;
-//
-//public class PalindromeTest {
-//    Palindrome palindrome;
-//
-//    @Before
-//    public void setUp() {
-//        // arrange
-//        this.palindrome = new Palindrome();
-//    }
-//
-//    @After
-//    public void tearDown() {
-//        this.palindrome = null;
-//    }
-//
-//    @BeforeClass
-//    public static void beforeClass() {
-//        System.out.println("Before Class");
-//    }
-//
-//    @AfterClass
-//    public static void afterClass() {
-//        System.out.println("After Class");
-//    }
-//
-//    @Test
-//    public void givenNonPositiveNumberShouldReturnError() {
-//        // act
-//
-//        //assert
-//    }
-////
+package com.stackroute.pe1;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class PalindromeTest {
+    Palindrome palindrome;
+
+    @Before
+    public void setUp() {
+        // arrange
+        this.palindrome = new Palindrome();
+    }
+
+    @After
+    public void tearDown() {
+        this.palindrome = null;
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Before Class");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("After Class");
+    }
+
+    @Test
+    public void givenNonPositiveNumberShouldReturnErrorMessage() {
+        // act
+        String actualResult = palindrome.checkPalindrome(-45);
+        // assert
+        assertEquals("Enter number greater than 0", actualResult);
+    }
+
+    @Test
+    public void givenNumberShouldReturnPalindromeNumber() {
+        // act
+        String actualResult = palindrome.checkPalindrome(4567654);
+        //assert
+        assertEquals("Palindrome",actualResult);
+    }
+
+    @Test
+    public void givenNumberShouldReturnNonPalindromeNumber() {
+        // act
+        String actualResult = palindrome.checkPalindrome(674345);
+        //assert
+        assertEquals("Not Palindrome",actualResult);
+    }
+
+    @Test
+    public void givenPalindromeNumberShouldReturnPalindromeNumberWithSumOfEvenNumberGreaterThan25() {
+        // act
+        String actualResult = palindrome.checkPalindrome(88688);
+        //assert
+        assertEquals("The number is palindrome and the sum of even numbers is greater than 25",actualResult);
+    }
+
+    @Test
+    public void givenPalindromeNumberShouldReturnPalindromeNumberWithSumOfEvenNumberLessThan25() {
+        // act
+        String actualResult = palindrome.checkPalindrome(123454321);
+        //assert
+        assertEquals("The number is palindrome and the sum of even numbers is not greater than 25",actualResult);
+    }
+}
